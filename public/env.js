ac89ecb4adde62be6a3df8bd76e61d2dbab41f2f
Katherine Otten Kim <kkatherine317@gmail.com>
2022-07-25 19:52:37 -0700

    fixed dotenv issue

---
+++ public/env.js
@@ -0,0 +1,4 @@
+window.env = {
+  "REACT_APP_GOOGLE_CLIENT_ID": "882192000932-s84n6fudpcnc0isfa2pojk2fnob39j12.apps.googleusercontent.com",
+  "REACT_APP_GOOGLE_SECRET": "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"
+};
