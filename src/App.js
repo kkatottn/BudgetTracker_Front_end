f48e603d3a458fd1dcd3affefae8d0e6e9617218
Katherine Otten Kim <kkatherine317@gmail.com>
2022-07-26 11:38:48 -0700

    change dotenv again2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [user, setUser] = useState(null);
   // const GOOGLE_CLIENT_ID =
   //   "882192000932-s84n6fudpcnc0isfa2pojk2fnob39j12.apps.googleusercontent.com";
-  const GOOGLE_CLIENT_ID = window.env.REACT_APP_GOOGLE_CLIENT_ID;
+  const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
   function handleCallbackResponse(response) {
     console.log("ID Token: " + response.credential);
