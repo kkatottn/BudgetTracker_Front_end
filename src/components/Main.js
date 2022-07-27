d4f7093662fd381a9e4159a713a3ac9e755cb51d
Katherine Otten Kim <kkatherine317@gmail.com>
2022-07-27 16:54:06 -0700

    add css main page to have same design with landing page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./Main.css";
 
 const Main = (props) => {
-  return <div>This is main page
-    <h1>this is user name: {props.user.name}</h1>
+  return <div id="main">
+    <header>Kash Note</header>
+    <main>
+      This is main page
+      <h1>this is user name: {props.user.name}</h1>
+    </main>
+    <footer>This is footer</footer>
   </div>;
 };
 
 export default Main;
+
+
