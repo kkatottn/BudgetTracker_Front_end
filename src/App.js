c1262563b99dc26a69b31d50f5179c04bf7ce155
Kelly Kang <shkx281@gmail.com>
2022-08-02 15:19:04 -0700

    completed writing axios calls.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
   const [userCategories, setUserCategories] = useState([]);
   const [budget, setBudget] = useState(null);
   const [expenses, setExpenses] = useState([]);
-  const [expenseData, setExpenseData] = useState({});
 
   const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
@@ -62,7 +61,7 @@ function App() {
       setUser(res)
     })
     .catch(()=>{
-      axios.post(`${URL}/user`, {'id_token': user.id_token, 'email': user.email, 'name': user.name})
+      axios.post(`${URL}/user`, {params:{'id_token': user.id_token, 'email': user.email, 'name': user.name}})
       .then((res) => {
         const newUser = {"user_id": res.id, "id_token": user.id_token, "email": user.email, "name": user.name}
         setUser(newUser)
@@ -78,49 +77,129 @@ function App() {
     // .then -> setDefaultCategories 
     // .catch -> return error msg
     axios.get(`${URL}/category`)
+    .then((res) => {
+      setDefaultCategories(res["default categories"]);
+    })
+    .catch(() => {
+      console.log("something wrong with get default categories!");
+    })
   }
+
 
   const getUserCategories = (user_id) => {
     // axios call to get user categories
     // .then -> setUserCategories
     // .catch -> return err msg
+    axios.get(`${URL}/${user_id}/category`, {params:{"month" : date.month, "year" : date.year}})
+    .then((res) => {
+      setUserCategories(res["user categories"]);
+    })
+    .catch(() => {
+      console.log("something wrong with get user categories!");
+    })
   }
 
-  const getBudget = (user_id, month, year) => {
+
+  const getBudget = (user_id) => {
     // axios call to get budget
     // .then -> setBudget
     // .catch -> return err msg 
+    axios.get(`${URL}/${user_id}/budget`,{params: {"month" : date.month, "year" : date.year}})
+    .then((res) => {
+      if ("amount" in res){
+        setBudget(res["amount"])  
+      }
+    })
+    .catch(() => {
+      console.log("something wrong with get user budget!");
+    })
   }
+
 
   const getExpenses = (user_id) => {
     // axios call to get expenses
     // .then -> setExpenses
     // .catch -> return .... 
+    axios.get(`${URL}/${user_id}/expense`, {params: {"id": user.user_id, "month": date.month, "year": date.year}})
+    .then((res) => {
+      setExpenses(res["user expenses"])
+    })
+    .catch(() => {
+      console.log("Something went wrong retrieving user expenses")
+    })
+    }
   }
 
-  const addExpense = (data) => {
+  const addExpense = (request_body) => {
+    // key => amount, category_id, description, month, year
     // axios post with data in request body
     // .then -> newExpenses = getExpenses
     // newExpense = {'description': data.descript}
     // setExpenses({expenses..., newExpense})
     //          setExpenses(newExpenses)
-
+    axios.post(`${URL}/${user.user_id}/expense`, request_body)
+    .then((res) => {
+      const newExpense = {
+        "amount" : request_body.amount,
+        "description" : res.description,
+        "month" : request_body.month,
+        "year" : request_body.year,
+        "category_id" : request_body.category_id
+      };
+      setExpenses([...expenses, newExpense]);
+    })
+    .catch(() => {
+      console.log("something wrong with add new expense!");
+    })
   }
+
   
-  const editExpense = (expense_id) => {
+  const editExpense = (expense_id, request_body) => {
     // axios patch with expense id
     // .then -> setExpense ({expense})......
-  }
+    axios.patch(`${URL}/expense/${expense_id}`, request_body)
+    .then(() => {
+      const modifiedExpenses = expenses.map((expense) => {
+        if (expense.expense_id === expense_id){
+          expense.description = request_body.description
+          expense.amount = request_body.amount
+        }
+        return expense;
+      });
+      setExpenses(modifiedExpenses);
+    })
+    .catch(() => {
+      console.log("something wrong with edit expense!");
+    })
+  };
 
-  const addBudget = (budgetData) => {
+
+  const addBudget = (request_body) => {
     // axios post with budget data in request body
     // .then -> setBudget
-  }
+    axios.post(`${URL}/${user.user_id}/budget`, request_body)
+    .then((res) => {
+      const newBudget = {"amount": res["amount"], "month": request_body.month, "year": request_body.year}
+      setBudget(newBudget)
+    })
+    .catch(() => {
+      console.log("Something went wrong trying to set a new budget!")
+    })
+  };
 
-  const editBudget = (budgetData) => {
+  const editBudget = (request_body) => {
     // axios patch with budget data in req body
+    // req body will have month, year, and amount
     // .then -> setBudget
-  }
+
+    axios.patch(`${URL}/${user.user_id}/budget`, request_body)
+    .then(()=> {
+      setBudget({...budget, "amount":request_body["amount"]})
+    })
+    .catch(() => {
+      console.log("Something went wrong editing the budget!")
+    })
+  };
 
 
 
